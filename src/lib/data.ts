22c6fea1fbd01154d4470b2c1ef13d13ba017100
King YT <gl3259807@gmail.com>
2025-07-14 13:09:50 +0000

    Bro dont do "Constantly Updated - Automatically Updates - Never left beh

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -69,8 +69,8 @@ export const products = [
     image: "https://skatesgarage.cc/images/SkatesBO6External.png",
     dataAiHint: "radar screen",
     features: [
-      { icon: "wrench" as keyof typeof Icons, text: "Constantly Updated - Automatically Updates - Never left behind" },
-      { icon: "zap" as keyof typeof Icons, text: "Powerful Chair - Strongest External - Has features you'd only see in a Internal" },
+      { icon: "wrench" as keyof typeof Icons, text: "Constantly Updated - Automatically Updates" },
+      { icon: "zap" as keyof typeof Icons, text: "Powerful Chair - Strongest External" },
       { icon: "cpu" as keyof typeof Icons, text: "Kernel-level Security - Operates at the highest level" },
       { icon: "shield" as keyof typeof Icons, text: "Lightweight & Powerful - The Swiss Knife for cheating" },
     ],
